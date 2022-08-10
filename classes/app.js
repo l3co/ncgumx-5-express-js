ddf5ce7d349cb23097fbde0eb62cf487847b28f4
leco <lcosta.sp.br@gmail.com>
2022-08-10 09:53:03 -0300

    refactor: :recycle: reorganize code

--- classes/app.js
+++ classes/app.js
@@ -1,13 +1,6 @@
-const http = require('http');
-// const routes = require('./routes')
 const express = require('express');
 
 const app = express();
-
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
-});
 
 app.use((req, res, next) => {
     console.log('In the next middleware')
