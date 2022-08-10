f96350723aa55bf2c27b9bb925e128c738915201
leco <lcosta.sp.br@gmail.com>
2022-08-10 12:35:32 -0300

    feat(middleware): :sparkles: configure post middleware

--- classes/app.js
+++ classes/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <h1>Add new Product</h1>
+        <form action="/product" method="POST">
+            <input type="text" name="title">
+            <button type="submit">ADD</button>
+        </form>
+    `);
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+});
+
 app.use((req, res, next) => {
     console.log('In the next middleware')
     res.send(`<h1>Hello from express</h1>`);
