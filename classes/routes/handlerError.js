97bda74cae7bf441279ee2b15d0668293d23cb82
leco <lcosta.sp.br@gmail.com>
2022-08-11 08:12:41 -0300

    feat(middleware): :rotating_light: create excpetion handler

---
+++ classes/routes/handlerError.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const router = Router()
+
+router.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+module.exports = router;
