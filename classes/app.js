14804a88c325c91635da9de210356ea100f459c9
leco <lcosta.sp.br@gmail.com>
2022-08-13 09:05:05 -0300

    feat(section-5): :art: exposed public path to using external files

--- classes/app.js
+++ classes/app.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 const routes = require('./routes')
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 routes(app);
 
 app.listen(3000);
